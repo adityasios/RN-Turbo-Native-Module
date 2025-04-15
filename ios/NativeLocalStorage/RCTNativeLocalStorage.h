5d271bf7bf8b756a47886e48f808d024da587df2
Inncrewin <aditya.kumar@inncrewin.com>
2025-04-15 09:32:12 +0530

    initial commit

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,16 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleExample
+//
+//  Created by inncrewin on 15/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.mm
@@ -0,0 +1,12 @@
+//
+//  RCTNativeLocalStorage.m
+//  TurboModuleExample
+//
+//  Created by inncrewin on 15/04/25.
+//
+
+#import "RCTNativeLocalStorage.h"
+
+@implementation RCTNativeLocalStorage
+
+@end
